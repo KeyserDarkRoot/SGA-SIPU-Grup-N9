0d837084b13c7fbecc702e6c586412c529975e49
KeyserDarkRoot <broshmario44@gmail.com>
2025-10-11 21:34:30 -0300

    Creacion de un archivo .py, nombrado sistema.py y Agregacion de clases generales del sistema

---
+++ sistema.py
@@ -0,0 +1,54 @@
+class RegistroNacional:
+    pass
+
+
+class Universidad:
+    pass
+
+
+class Carrera:
+    pass
+
+
+class Periodo:
+    pass
+
+
+class OfertaAcademica:
+    pass
+
+
+class Sede:
+    pass
+
+
+class Laboratorio:
+    pass
+
+
+class Aspirante:
+    pass
+
+
+class Postulacion:
+    pass
+
+
+class Inscripcion:
+    pass
+
+
+class Evaluacion:
+    pass
+
+
+class Puntaje:
+    pass
+
+
+class Monitor:
+    pass
+
+
+class Notificacion:
+    pass
