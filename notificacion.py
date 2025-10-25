82fbb4763a47966b265ec437925eefd5e2500637
Brithanymt <isaacyagual71@gmail.com>
2025-10-25 18:00:37 -0500

    Refactorizados constructor y métodos de Notificacion

--- notificacion.py
+++ notificacion.py
@@ -1,12 +1,13 @@
-class Notificacion:
-     def _init_(self, idNotificacion, mensaje, fecha_envio, estado):
+class Notificacion: 
+    def __init__(self, idNotificacion, mensaje, fecha_envio, estado):
         self.__idNotificacion = idNotificacion
         self.mensaje = mensaje
         self.fecha_envio = fecha_envio
         self.estado = estado
-        
-        def enviar(self):
-            print("Enviando correo al aspirante: ")
-            def marcarLeido(self):
-                print("Marcando correo como leído ")
-                
+
+    def enviar(self):
+        print(f"Enviando notificación: {self.mensaje}")
+
+    def marcar_leido(self):
+        self.estado = "leído"
+        print("Notificación marcada como leída")
