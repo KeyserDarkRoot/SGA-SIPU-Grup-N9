15292bcb0dc30d1e16ed2411c384d0d1eac429d0
LagoMatthew <matthewlalex021@gmail.com>
2025-10-26 15:37:29 -0500

    Se modifico los metodos de la clase postilacion

--- postulacion.py
+++ postulacion.py
@@ -23,12 +23,20 @@ class Postulacion:
         self.carreraSeleccionada = carreraSeleccionada
         self.registro = registro
 
-    def registrarOpciones(self): 
-        print("ERegistro exitoso") 
+    def registrar_opciones(self):
+        print(f"Registro exitoso: {self.opciones_carrera}")
 
-    def calcularPuntajeFinal(self): 
-        print("Calculando puntaje.") 
+    def calcular_puntaje_final(self):
+        puntaje_base = 600
+        puntaje_prioridad = self.prioridad * 50
+        puntaje_total = puntaje_base + puntaje_prioridad
+        print(f"Puntaje final: {puntaje_total}")
+        return puntaje_total
 
-    def GenerarCertificadoPostulacion(self): 
-        print("Generando certificados de evaluacion....")
-
+    def generar_certificado_postulacion(self):
+        print(f"Generando certificado para la carrera {self.carrera_seleccionada}")
+        if self.registro:
+            datos = self.registro.obtener_datos_postulante(self.id_postulacion)
+            print(f"Datos del postulante: {datos}")
+        else:
+            print("Registro único no disponible.")
