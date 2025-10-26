12b8add830da23bbd4869ba15e71d0256bbbc3f6
LagoMatthew <matthewlalex021@gmail.com>
2025-10-26 15:17:44 -0500

    Agregacion de interfaz con un metodo abstracto y una clase de simulacion, se agrego tambien el atributo registro

--- postulacion.py
+++ postulacion.py
@@ -1,15 +1,34 @@
+from abc import ABC, abstractmethod
+
+#Interfaz de certificados
+class ICertificado(ABC):
+    @abstractmethod
+    def generar_certificado_postulacion(self):
+        pass
+
+class RegistroUnico:
+    def obtener_datos_postulante(self, id_postulacion):
+        return {
+            "nombre": "Juan Pérez",
+            "puntaje": 850,
+            "documento": "1728392010"
+        }
+
 class Postulacion:
-    def __init__(self, idPostulacion,opcionesCarrera,prioridad,carreraSeleccionada):
+    def __init__(self, idPostulacion,opcionesCarrera,prioridad,carreraSeleccionada,registro):
+        
         self.idPostulacion = idPostulacion
         self.opcionesCarrera = opcionesCarrera
         self.prioridad = prioridad
         self.carreraSeleccionada = carreraSeleccionada
+        self.registro = registro
 
-    def registrarOpciones(self):
-        print("ERegistro exitoso")
+    def registrarOpciones(self): 
+        print("ERegistro exitoso") 
 
-    def calcularPuntajeFinal(self):
-        print("Calculando puntaje.")
+    def calcularPuntajeFinal(self): 
+        print("Calculando puntaje.") 
 
-    def GenerarCertificadoPostulacion(self):
+    def GenerarCertificadoPostulacion(self): 
         print("Generando certificados de evaluacion....")
+
