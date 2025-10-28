dfe5cedac9163c2d900a66a1884964ef0abb016d
KeyserDarkRoot <broshmario44@gmail.com>
2025-10-27 23:35:14 -0300

    Agregacion de un archivo main.py para respectivas pruebas

---
+++ main.py
@@ -0,0 +1,25 @@
+from periodo import Periodo
+from ofertaAcademica import OfertaAcademica
+from carreras_universidad import Carrera
+
+# Crear y activar un periodo
+p1 = Periodo(1, "2025A", "2025-01-01", "2025-06-30")
+p1.crear_periodo()
+p1.activar_periodo()
+
+# Crear oferta académica dentro del periodo activo
+oferta = OfertaAcademica(1, "Universidad Central", [], "2025-01-10", "activa")
+oferta.crear_oferta()
+
+# Crear carreras
+c1 = Carrera(1, "Ingeniería en Software", "Facultad de Ingeniería", "Presencial", "8 semestres", 50)
+c2 = Carrera(2, "Diseño Web", "Facultad de Artes", "Virtual", "6 semestres", 30)
+
+# Agregar carreras a la oferta
+oferta.agregarCarrera(c1)
+oferta.agregarCarrera(c2)
+
+# Cerrar un periodo
+p1 = Periodo(1, "2025A", "2025-01-01", "2025-06-30")
+p1.cerrar_periodo()
+
