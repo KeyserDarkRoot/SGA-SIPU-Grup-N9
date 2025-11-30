7bdffe6a377c843b5e8e0f096d575bb4abd12643
KeyserDarkRoot <broshmario44@gmail.com>
2025-11-30 15:44:21 -0500

    Inplementacion de prueba

--- prueba_clase_inscripcion.py
+++ prueba_clase_inscripcion.py
@@ -8,14 +8,14 @@ inscripcion = Inscripcion(
     periodo_id="2025A",
     ies_id=101,
     tipo_documento="cédula",
-    identificacion="0102030405",
+    identificacion="01020304033",
     nombres="Carlos Andrés",
     apellidos="Mendoza Rojas",
     carrera_seleccionada="Ingeniería Civil"
 )
 
 if inscripcion.validar_registro_nacional(cliente):
-    if inscripcion.validar_periodo(periodo):
+    if inscripcion.validar_periodo():
         inscripcion.guardar_en_supabase(cliente)
     else:
         print("El periodo no está activo o fuera de fecha.")
